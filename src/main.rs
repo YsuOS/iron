f208c916b33e6a71f51a0e87d2591889d9987224
Ryosuke YASUOKA <yasuoka0830@gmail.com>
2019-06-10 00:34:55 +0900

    add router crate fix main.rc

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 extern crate iron;
+extern crate router;
 
 use iron::prelude::*;
 use iron::status;
+use router::Router;
 
 fn main() {
-    Iron::new(hello_world).http("localhost:3000").unwrap();
+    let mut router = Router::new();
+    router.get("/", hello_world, "root");
+    router.get("/:query", hello_world, "query");
+
+    Iron::new(router).http("localhost:3000").unwrap();
+
+    fn hello_world(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, "Hello world")))
+    }
 }
 
 
-fn hello_world(_: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, "Hello world")))
-}
