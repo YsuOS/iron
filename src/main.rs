10ff877b0a57a71ff0ad1a5440f00512653be648
yasuoka ryosuke <yasuoka0830@gmail.com>
2019-06-10 01:30:51 +0900

    fix Cargo.toml add query_handler

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,19 @@ use router::Router;
 
 fn main() {
     let mut router = Router::new();
-    router.get("/", hello_world, "root");
-    router.get("/:query", hello_world, "query");
+    router.get("/", handler, "handler");
+    router.get("/:query", query_handler, "query_handler");
 
     Iron::new(router).http("localhost:3000").unwrap();
 
-    fn hello_world(_: &mut Request) -> IronResult<Response> {
+    fn handler(_: &mut Request) -> IronResult<Response> {
         Ok(Response::with((status::Ok, "Hello world")))
+    }
+
+    fn query_handler(req: &mut Request) -> IronResult<Response> {
+        let ref query = req.extensions.get::<Router>()
+            .unwrap().find("query").unwrap_or("/");
+        Ok(Response::with((status::Ok, *query)))
     }
 }
 
